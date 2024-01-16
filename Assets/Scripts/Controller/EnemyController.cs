0dccedb44ce457711d97886cacb971761ca6345a
Nikita <Shamray>
2024-01-16 20:54:16 +0200

    add enemy spawn logic

--- Assets/Scripts/Controller/EnemyController.cs
+++ Assets/Scripts/Controller/EnemyController.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using DefaultNamespace.Handler;
 using DefaultNamespace.Hero;
 using DefaultNamespace.Interfaces;
 using DefaultNamespace.Manager;
@@ -11,19 +13,32 @@ namespace DefaultNamespace.Controller
 {
   public class EnemyController : object, IInitialize
   {
+    private const int BLUE_ENEMY_RATIO = 1;
+    private const int RED_ENEMY_RATIO = 4;
+    
     private const int MAX_ENEMY_COUNT = 30;
     private const int START_ENEMY_COUNT = 5;
+    
+    private const float INITIAL_SPAWN_INTERVAL = 30f;
+    private const float MIN_SPAWN_INTERVAL = 6.0f;
+    private const float SPAWN_INTERVAL_DECREMENT = 2.0f;
+    
     public event Action<HeroEnemy> OnSpawnEnemy;
 
     private readonly List<HeroEnemy> _enemies = new List<HeroEnemy>();
     public List<HeroEnemy> Enemies => _enemies;
 
-    private SpawnManager _spawnManager;
+    private readonly CoroutineHandler _coroutineHandler;
+    private readonly SpawnManager _spawnManager;
+    private float _currentSpawnInterval;
 
     [Inject]
-    private void Construct (SpawnManager spawnManager)
+    private EnemyController (
+      SpawnManager spawnManager, 
+      CoroutineHandler coroutineHandler)
     {
       _spawnManager = spawnManager;
+      _coroutineHandler = coroutineHandler;
     }
 
     public void Initialize()
@@ -35,6 +50,8 @@ namespace DefaultNamespace.Controller
         VARIABLE.Initialize();
       }
 
+      _currentSpawnInterval = INITIAL_SPAWN_INTERVAL;
+      _coroutineHandler.StartRoutine(SpawnEnemyRoutine());
       IsInitialized = true;
     }
 
@@ -48,7 +65,7 @@ namespace DefaultNamespace.Controller
         int totalBlueEnemies = Enemies.Count(enemy => enemy.Type == HeroType.EnemyBlue);
         int totalRedEnemies = Enemies.Count(enemy => enemy.Type == HeroType.EnemyRed);
 
-        return totalBlueEnemies < totalRedEnemies * 4 ? HeroType.EnemyBlue : HeroType.EnemyRed;
+        return totalBlueEnemies < totalRedEnemies * RED_ENEMY_RATIO ? HeroType.EnemyBlue : HeroType.EnemyRed;
       }
     }
 
@@ -56,9 +73,6 @@ namespace DefaultNamespace.Controller
     {
       for (int index = 0; index < count; index++)
       {
-        const int BLUE_ENEMY_RATIO = 1;
-        const int RED_ENEMY_RATIO = 4;
-
         const int TOTAL_ENEMY_RATIO = BLUE_ENEMY_RATIO + RED_ENEMY_RATIO;
 
         HeroType enemyType = (index % TOTAL_ENEMY_RATIO == 0) ? HeroType.EnemyBlue : HeroType.EnemyRed;
@@ -82,6 +96,18 @@ namespace DefaultNamespace.Controller
       enemy.name = $"{GameConstants.ENEMY_NAME} {index} {enemyType}";
       
       OnSpawnEnemy?.Invoke(enemy);
+    }
+    
+    private IEnumerator SpawnEnemyRoutine()
+    {
+      while (true)
+      {
+        yield return new WaitForSeconds(_currentSpawnInterval);
+        
+        _currentSpawnInterval = Mathf.Max(MIN_SPAWN_INTERVAL, _currentSpawnInterval - SPAWN_INTERVAL_DECREMENT);
+        
+        GenerateEnemy();
+      }
     }
 
     public bool IsInitialized
--- Assets/Scripts/ScriptableObjects/GameConfig.cs
+++ Assets/Scripts/ScriptableObjects/GameConfig.cs
@@ -5,12 +5,6 @@ namespace DefaultNamespace.ScriptableObjects
   [CreateAssetMenu(fileName = "GameConfig", menuName = "ScriptableObject/GameConfig")]
   public class GameConfig : ScriptableObject
   {
-    [Header("Player Stats")]
-    public int initialPlayerHealth = 100;
-    public int initialPlayerStrength = 50;
-    public int strengthPerBlueKill = 50;
-    public int strengthPerRedKill = 15;
-
     [Header("Projectile")]
     public float projectileChanceToBounce = 0.3f;
     public float projectileBounceStrengthGain = 10f;
@@ -25,10 +19,6 @@ namespace DefaultNamespace.ScriptableObjects
     [Header("Enemy Stats")]
     public int initialBlueEnemyHealth = 100;
     public int initialRedEnemyHealth = 50;
-
-    /*[Header("Enemy Types")]
-    public BlueEnemy blueEnemyPrefab;
-    public RedEnemy redEnemyPrefab;*/
 
     [Header("Enemy Spawn")]
     public float initialSpawnInterval = 10f;
