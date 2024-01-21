9c6da14fa710742f8d33ef1deed01b52fe3ad7cc
Nikita <Shamray>
2024-01-21 19:56:42 +0200

    add ProjectileBase logic

--- Assets/Scripts/Projectile/ProjectileBase.cs
+++ Assets/Scripts/Projectile/ProjectileBase.cs
@@ -6,16 +6,24 @@ using UnityEngine;
 
 namespace DefaultNamespace.Projectile
 {
-  public abstract class ProjectileBase : MonoBehaviour
+  public class ProjectileBase : MonoBehaviour
   {
     private const float MAX_LIFE_TIME = 5f;
     public event Action OnShoot;
+    
+    private readonly List<Collider> _ignoredColliders = new List<Collider>();
 
     [SerializeField]
-    private Rigidbody _rigidbody;
+    private float _speed = 20f;
+    [SerializeField]
+    private LayerMask _hittableLayers;
+    [SerializeField]
+    private CapsuleCollider _capsuleCollider;
 
     private AttackInfo _attackInfo;
-    private readonly List<Collider> _ignoredColliders = new List<Collider>();
+    private float _shootTime;
+    private Vector3 _velocity;
+    private Vector3 _lastPosition;
 
     private void Awake()
     {
@@ -25,6 +33,10 @@ namespace DefaultNamespace.Projectile
     public void Shoot (AttackInfo attackInfo)
     {
       _attackInfo = attackInfo;
+      
+      _shootTime = Time.time;
+      _lastPosition = transform.position;
+      _velocity = transform.forward * _speed;
 
       var heroBase = (_attackInfo.DamageDealer as ComponentBase)?.ComponentOwner;
 
@@ -37,6 +49,46 @@ namespace DefaultNamespace.Projectile
       OnShoot?.Invoke();
     }
 
+    void Update()
+    {
+      transform.position += _velocity * Time.deltaTime;
+      transform.forward = _velocity.normalized;
+      
+      RaycastHit closestHit = new RaycastHit();
+      closestHit.distance = Mathf.Infinity;
+      bool foundHit = false;
+
+      Vector3 displacementSinceLastFrame = transform.position - _lastPosition;
+
+      RaycastHit [] hits = Physics.SphereCastAll(_lastPosition, Radius(), displacementSinceLastFrame.normalized, displacementSinceLastFrame.magnitude, _hittableLayers);
+
+      List<IDamagable> damagables = new List<IDamagable>();
+
+      foreach (var hit in hits)
+      {
+        if (IsHitValid(hit, out damagables) && hit.distance < closestHit.distance)
+        {
+          foundHit = true;
+          closestHit = hit;
+        }
+      }
+
+      if (foundHit)
+      {
+        if (closestHit.distance <= 0f)
+        {
+          closestHit.point = transform.position;
+          closestHit.normal = -transform.forward;
+        }
+
+        OnHit(damagables);
+      }
+      
+      Debug.DrawLine(_lastPosition, transform.position, Color.blue);
+
+      _lastPosition = transform.position;
+    }
+    
     private bool IsHitValid (RaycastHit hit, out List<IDamagable> damagables)
     {
       damagables = new List<IDamagable>();
@@ -47,11 +99,6 @@ namespace DefaultNamespace.Projectile
       }
 
       if (!hit.collider.gameObject.TryGetComponent(out IDamagable damagable))
-      {
-        return false;
-      }
-
-      if (damagable == null)
       {
         return false;
       }
@@ -75,6 +122,12 @@ namespace DefaultNamespace.Projectile
       }
 
       Destroy(gameObject);
+    }
+
+    private float Radius()
+    {
+      Vector3 capsuleColliderScale = _capsuleCollider.transform.localScale;
+      return Mathf.Max(_capsuleCollider.radius * capsuleColliderScale.x, _capsuleCollider.radius * capsuleColliderScale.y, _capsuleCollider.radius * capsuleColliderScale.z);
     }
   }
 }
